6f9523e53476e588d5b97631254c4d8dbb6d621f
Brandon Copeland <br@ndoncopeland.com>
2013-04-23 20:20:29 -0500

    updates symbols on crime type renderer

--- js/map/crimetyperenderer.js
+++ js/map/crimetyperenderer.js
@@ -1,13 +1,41 @@
 define(['dojo', 'esri/renderers/UniqueValueRenderer', 'esri/symbols/SimpleMarkerSymbol'], function (dojo, UniqueValueRenderer, SimpleMarkerSymbol) {
 	'use strict';
 
-	var defaultSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 14, null, new dojo.Color([35, 232, 86, 0.7]));
+	var defaultSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 14, null, new dojo.Color([80, 80, 80, 0.7]));
 	var renderer = new UniqueValueRenderer(defaultSymbol, 'type');
 
 	renderer.addValue({
 		value: 'Driveway Robbery',
 		symbol: new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 14, null, new dojo.Color([232, 35, 116, 0.7])),
 		label: 'Driveway Robbery',
+		description: ''
+	});
+
+	renderer.addValue({
+		value: 'Store Robbery',
+		symbol: new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 14, null, new dojo.Color([35, 149, 232, 0.7])),
+		label: 'Store Robbery',
+		description: ''
+	});
+
+	renderer.addValue({
+		value: 'Home Burglary',
+		symbol: new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 14, null, new dojo.Color([35, 232, 49, 0.7])),
+		label: 'Home Burglary',
+		description: ''
+	});
+
+	renderer.addValue({
+		value: 'Car Break-in',
+		symbol: new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 14, null, new dojo.Color([232, 126, 35, 0.7])),
+		label: 'Car Break-in',
+		description: ''
+	});
+
+	renderer.addValue({
+		value: 'Suspect Chase',
+		symbol: new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 14, null, new dojo.Color([232, 35, 215, 0.7])),
+		label: 'Suspect Chase',
 		description: ''
 	});
 
